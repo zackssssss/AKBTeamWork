a8c0ad83dff25b44ded6116edbe88fc584977a5b
ChickenPie <278306050@qq.com>
2019-08-18 23:25:29 +0800

    修改了member_list页面tab的切换效果为淡入淡出

--- web/js/index.js
+++ web/js/index.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
   
+  // var tabHeight = $('.tab.active').height();
+  // function animateTabHeight() {
+  //     tabHeight = $('.tab.active').height();
+  //     $('.tab-content').stop().css({ height: tabHeight + 'px' });
+  // }
+  // animateTabHeight();
+
   var $wrapper = $('.tab-wrapper'),
       $allTabs = $wrapper.find('.tab-content > div'),
       $tabMenu = $wrapper.find('.tab-menu li'),
       $line = $('<div class="line"></div>').appendTo($tabMenu);
   
-  $allTabs.not(':first-of-type').hide();  
+  $allTabs.not(':first-of-type').fadeOut(); 
   $tabMenu.filter(':first-of-type').find(':first').width('100%')
   
   $tabMenu.each(function(i) {
@@ -23,11 +30,13 @@ $(document).ready(function() {
     
     $getWrapper.find($tabMenu).removeClass('active');
     $(this).addClass('active');
+    // animateTabHeight();
     
     $getWrapper.find('.line').width(0);
     $(this).find($line).animate({'width':'100%'}, 'fast');
-    $getWrapper.find($allTabs).hide();
-    $getWrapper.find($allTabs).filter('[data-tab='+dataTab+']').show();
+    $getWrapper.find($allTabs).fadeOut();
+    $getWrapper.find($allTabs).filter('[data-tab='+dataTab+']').fadeIn();
+    // animateTabHeight();
   });
 
 });//end ready
